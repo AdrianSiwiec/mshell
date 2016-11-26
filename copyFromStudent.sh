f0b17e79aff99d07572842db21b80475eaf83ac5
Adrian Siwiec <adrian.siwiec@gmail.com>
2016-11-26 17:59:20 +0100

    Cleaned repo

--- copyFromStudent.sh
+++
@@ -1,2 +0,0 @@
-scp -r z1123375@student.tcs.uj.edu.pl:/home/z1123375/afs/profil_TCS/Desktop/Systemy/ ./CopyFromStudent
-
--- copyToStudent.sh
+++
@@ -1,2 +0,0 @@
-ls | grep -v .sh | xargs sh -c 'scp -r "$@" z1123375@student.tcs.uj.edu.pl:/home/z1123375/afs/profil_TCS/Desktop/Systemy/' sh {} +
-
